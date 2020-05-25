03e7cd6d65ef7c0a82106e2edd840b681b82682a
kamiljan199 <kamiljan199@gmail.com>
2020-05-25 21:11:02 +0200

    Refactor of enemies and first version of fireball.

--- Prototype/Assets/Scripts/Enemies/BossSpawnerController.cs
+++ Prototype/Assets/Scripts/Enemies/BossSpawnerController.cs
@@ -4,65 +4,15 @@ using System.Linq;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class BossSpawnerController : MonoBehaviour
+public class BossSpawnerController : EnemyController
 {
-    private Rigidbody rb;
-    public float speed = 5.0f;
-    public float playerInRange = 15.0f;
-    public float stoppingDistance = 7.5f;
-    public float retreatDistance = 5.0f;
-    public GameObject player;
-    public float maxHealth = 30.0f;
-
-    public Color defaultColor;
-    public Color damagedColor;
-    public float dmgAnimationDuration = 0.5f;
-    private float lastDmgTime = 0.0f;
-
-    public float slow = 0.0f;
-    public float mudSlow = 0.0f;
-    public float frozenTo = 0.0f;
-
-    private float enemyHealth;
-
-    public CharController charController;
-    public HealthBar healthBar;
-    //private bool pushedEnemiess;
-
-
-
-    //attacking
-    //private float meleeRange = 1.5f;
-    //public float attackDelay = 1.0f;
-    //private float timestampAttack;
-    //private float timestampAfterAttackPushStart;
-    //private float timestampAfterAttackPushStop;
-    //private bool isPushedAfterAttack;
-    //public float pushBackForceAfterAttack = 5.0f;
-    //public float damage = 1.0f;
+    public float retreatDistance = 0.75f;
 
     private float timestampSpawn;
     public float spawnDelay = 3.0f;
     public GameObject enemy;
     public int enemyWaveNumber = 3;
     private int startWaveNumber = 0;
-
-    bool isPoisoned;
-    public float poisonCooldownTime = 1.0f;
-    float nextPoisonTime = 0.0f;
-    float poisoningStart = -1.0f;
-    float poisoningEnd = -1.0f;
-
-    bool isBurnt;
-    public float burningCooldownTime = 1.0f;
-    float nextBurnTime = 0.0f;
-
-    //public bool isMelee = false;
-
-    //public GameObject arrow;
-    //public float arrowSpeed = 1.0f;
-    //public float shootingRange = 10.0f;
-    //public float shootingDelay = 3.0f;
 
     bool isMoving = false;
     private float timestampMovement = 2.0f;
@@ -80,6 +30,9 @@ public class BossSpawnerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        charController = GameManager.Instance.player;
+        player = charController.gameObject;
+
         rb = GetComponent<Rigidbody>();
         //timestampAttack = 0.0f;
         //timestampAfterAttackPushStart = 0.0f;
@@ -282,107 +235,6 @@ public class BossSpawnerController : MonoBehaviour
         transform.LookAt(player.transform);
     }
     
-    void Stop()
-    {
-        rb.velocity = Vector3.zero;
-    }
-    
-    public void TakeDamage(float value)
-    {
-        lastDmgTime = Time.time;
-        enemyHealth -= value;
-        healthBar.SetHealth(enemyHealth);
-
-        if (enemyHealth %2 == 0 && enemyHealth != maxHealth && enemyHealth > 0)
-        {
-            startWaveNumber = 0;
-        }
-        if (enemyHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
-    /*
-    void ShootWithArrow()
-    {
-        Vector3 arrowPosition = transform.position;
-        arrowPosition.y += 2.0f;
-
-        Quaternion arrowRotation = transform.rotation;
-        //arrowRotation.x += 30.0f;
-
-        Vector3 direction = ((Vector3)player.transform.position - transform.position).normalized;
-        //direction = Vector3.Normalize(direction.normalized + Vector3.up * 0.75f);
-
-        GameObject newArrow = GameObject.Instantiate(arrow, arrowPosition, arrowRotation);
-        newArrow.SetActive(true);
-        Rigidbody rbArrow = newArrow.GetComponent<Rigidbody>();
-        //newArrow.GetComponent<Rigidbody>().AddForce(direction * arrowSpeed, ForceMode.Impulse);
-        //rbArrow.AddForce(Vector3.forward * arrowSpeed, ForceMode.Impulse);
-        Debug.Log("velocity here: " + rb.velocity);
-        rbArrow.velocity = direction * arrowSpeed;
-        Debug.Log("velocity here: " + rb.velocity);
-    }
-    
-    public void PushEnemy(Vector3 direction, float force)
-    {
-        //direction = ((Vector3)transform.position - player.transform.position).normalized;
-
-
-        //rb.MovePosition(transform.position + direction * speed * Time.deltaTime * 60.0f);
-        rb.AddForce(direction * force, ForceMode.VelocityChange);
-    }
-    */
-    private void SetMeshColor(Color color)
-    {
-        Renderer[] renderers = GetComponentsInChildren<Renderer>();
-        for (int i = 0; i < renderers.Length; i++)
-        {
-            renderers[i].material.SetColor("_BaseColor", color);
-        }
-    }
-
-    private void CalculateColor()
-    {
-        float R = Time.time - lastDmgTime;
-        if (R <= dmgAnimationDuration)
-        {
-            float halfDuration = dmgAnimationDuration / 2.0f;
-            if (R > halfDuration) R = dmgAnimationDuration - R;
-            float wsp = R / halfDuration;
-            SetMeshColor(((1.0f - wsp) * defaultColor) + (wsp * damagedColor));
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Mud"))
-        {
-            mudSlow = 2.0f;
-        }
-        if (other.CompareTag("MudOut"))
-        {
-            mudSlow = 0.0f;
-        }
-        if (other.CompareTag("ToxicFog"))
-        {
-            isPoisoned = true;
-        }
-        if (other.CompareTag("ToxicFogOut") && isPoisoned == true)
-        {
-            isPoisoned = false;
-            poisoningStart = Time.time;
-            poisoningEnd = Time.time + 2.0f;
-        }
-        if (other.CompareTag("Fire"))
-        {
-            isBurnt = true;
-        }
-        if (other.CompareTag("FireOut"))
-        {
-            isBurnt = false;
-        }
-    }
 
     string output;
 
---
+++ Prototype/Assets/Scripts/Enemies/RangerController.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangerController : EnemyController
+{
+    public float retreatDistance = 0.75f;
+
+    public GameObject arrow;
+    public float arrowSpeed = 10.0f;
+    public float shootingRange = 10.0f;
+    public float shootingDelay = 3.0f;
+    public bool isMoving = false;
+
+    public bool enemyTriggered = false;
+    public Quaternion startRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GameManager.Instance.player;
+        player = charController.gameObject;
+        rb = GetComponent<Rigidbody>();
+        timestampAttack = 0.0f;
+        timestampAfterAttackPushStart = 0.0f;
+        timestampAfterAttackPushStop = 0.0f;
+        enemyHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+        SetMeshColor(defaultColor);
+        startRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((isPoisoned) && Time.time >= nextPoisonTime)
+        {
+            TakeDamage(0.5f);
+            nextPoisonTime = Time.time + poisonCooldownTime;
+        }
+        if ((poisoningEnd >= Time.time && Time.time >= poisoningStart) && Time.time >= nextPoisonTime)
+        {
+            TakeDamage(0.5f);
+            nextPoisonTime = Time.time + poisonCooldownTime;
+        }
+        if ((isBurnt) && Time.time >= nextBurnTime)
+        {
+            TakeDamage(1.0f);
+            nextBurnTime = Time.time + burningCooldownTime;
+        }
+
+        CalculateColor();
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
+        Vector3 pushDirection = Vector3.zero;
+        
+
+        if (Time.time > frozenTo)
+        {
+                if (Vector3.Distance(transform.position, player.transform.position) <= playerInRange && Vector3.Distance(transform.position, player.transform.position) > stoppingDistance)
+                {
+                    ChasePlayer();
+                    isMoving = true;
+                   
+                }
+                else if (Vector3.Distance(transform.position, player.transform.position) <= playerInRange && Vector3.Distance(transform.position, player.transform.position) < stoppingDistance
+                                                                && Vector3.Distance(transform.position, player.transform.position) > retreatDistance)
+                {
+                    MoveToGetBetterPosition();
+                    isMoving = false;
+                }
+                else if (Vector3.Distance(transform.position, player.transform.position) < retreatDistance)
+                {
+                    RunAwayFromPlayer();
+                    isMoving = true;
+                }
+                else if (Vector3.Distance(transform.position, player.transform.position) <= stoppingDistance)
+                {
+                    Stop();
+                    isMoving = false;
+                }
+        }
+
+
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= shootingRange)
+        {
+            CheckIfEnemySeePlayer();
+            if (timestampAttack <= Time.time && enemyTriggered == true)
+            {
+                timestampAttack = Time.time + shootingDelay;
+                if (isMoving == false)
+                {
+                    ShootWithArrow();
+                }
+            }
+        }
+    }
+
+    void RunAwayFromPlayer()
+    {
+        Vector3 direction = ((Vector3)player.transform.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(-direction.x, 0, -direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        //transform.position = Vector3.Slerp(transform.position, transform.position + direction * speed, Time.deltaTime);
+        rb.MovePosition(transform.position + direction * (-speed - slow - mudSlow) * Time.deltaTime);
+    }
+
+    void CheckIfEnemySeePlayer()
+    {
+        var heading = player.transform.position - transform.position;
+        var distance = heading.magnitude;
+        var direction = (heading / distance);
+        RaycastHit hit;
+        //Debug.Log(hit.collider.gameObject);
+        Debug.DrawRay(transform.position, direction);
+
+        if (Vector2.Distance(transform.position, player.transform.position) < playerInRange)
+        {
+            if (Physics.Raycast(transform.position, direction, out hit, playerInRange))
+            {
+                if (hit.collider != null && hit.collider.gameObject == player)
+                {
+                    enemyTriggered = true;
+                }
+                else enemyTriggered = false;
+            }
+        }
+    }
+
+    void MoveToGetBetterPosition()
+    {
+        if (enemyTriggered == false && Vector3.Distance(transform.position, player.transform.position) < stoppingDistance && Vector3.Distance(transform.position, player.transform.position) > (retreatDistance + 0.5f))
+        {
+            startRotation = Quaternion.AngleAxis(-90, transform.forward);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, startRotation, speed / 10 * Time.deltaTime);
+
+            //Vector3 direction = Vector3.right;
+            rb.MovePosition(transform.position + Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    void ShootWithArrow()
+    {
+        Vector3 arrowPosition = transform.position;
+        arrowPosition.y += 2.0f;
+
+        Quaternion arrowRotation = transform.rotation;
+        //arrowRotation.x += 30.0f;
+
+        Vector3 direction = ((Vector3)player.transform.position - transform.position).normalized;
+        //direction = Vector3.Normalize(direction.normalized + Vector3.up * 0.75f);
+
+        GameObject newArrow = GameObject.Instantiate(arrow, arrowPosition, arrowRotation);
+        newArrow.SetActive(true);
+        Rigidbody rbArrow = newArrow.GetComponent<Rigidbody>();
+        rbArrow.AddForce(direction * arrowSpeed, ForceMode.Impulse);
+        //rbArrow.velocity = Vector3.zero;
+        //rbArrow.velocity = direction * arrowSpeed;
+    }
+
+}
