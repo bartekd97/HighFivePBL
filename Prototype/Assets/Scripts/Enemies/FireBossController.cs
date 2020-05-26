6d1f10feec578d2cbfa7286ef15c7e5c18cc2ad8
kamiljan199 <kamiljan199@gmail.com>
2020-05-26 18:46:12 +0200

    Added FireBoss

--- Prototype/Assets/Scripts/Enemies/FireBossController.cs
+++ Prototype/Assets/Scripts/Enemies/FireBossController.cs
@@ -9,11 +9,18 @@ public class FireBossController : EnemyController
     public GameObject firewall;
     public GameObject preFireball;
     public GameObject fireball;
-    public GameObject clearFire;
+
+    public GameObject thrower;
 
 
-    public float burningCooldownTime = 4.0f;
-    public float nextBurnTime = 0.0f;
+    public float attackCooldownTime = 5.0f;
+    public float nextAttackTime = 0.0f;
+
+    public float fireWallRange = 10.0f;
+
+    public float retreatDistance = 6.75f;
+
+
 
     void Start()
     {
@@ -29,6 +36,7 @@ public class FireBossController : EnemyController
         SetMeshColor(defaultColor);
         //timestampMovement = 0.0f;
         ThrowFireball();
+
     }
 
     // Update is called once per frame
@@ -54,53 +62,66 @@ public class FireBossController : EnemyController
 
         Vector3 pushDirection = Vector3.zero;
 
-
-        if (nextBurnTime < Time.time && Vector3.Distance(transform.position, player.transform.position) <= playerInRange)
+        if(cellNumber == GameManager.Instance.currentCell.CellSiteIndex)
         {
-            if(true)
+            LookAtPlayer();
+            if (nextAttackTime < Time.time && Vector3.Distance(transform.position, player.transform.position) <= playerInRange)
             {
-
+                if (Vector3.Distance(transform.position, player.transform.position) <= fireWallRange)
+                {
+                    ThrowFirewall();
+                }
+                else
+                {
+                    ThrowFireball();
+                }
+                nextAttackTime = attackCooldownTime + Time.time;
             }
-
-
-
-
-            nextBurnTime = burningCooldownTime + Time.time;
+            if (Vector3.Distance(transform.position, player.transform.position) < retreatDistance)
+            {
+                RunAwayFromPlayer();
+            }
         }
-  
 
-      
+    }
+
+    void RunAwayFromPlayer()
+    {
+        UnityEngine.Debug.Log("test");
+
+        Vector3 direction = ((Vector3)player.transform.position - transform.position).normalized;
+        //Quaternion lookRotation = Quaternion.LookRotation(new Vector3(-direction.x, 0, -direction.z));
+        //transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+        //transform.position = Vector3.Slerp(transform.position, transform.position + direction * speed, Time.deltaTime);
+        rb.MovePosition(transform.position + direction * (-speed - slow - mudSlow) * Time.deltaTime);
+    }
+
+    void LookAtPlayer()
+    {
+        transform.LookAt(new Vector3(player.transform.position.x, 1.0f, player.transform.position.z));
     }
 
     private IEnumerator Fireball(float waitTime, Vector3 fireballPosition)
     {
        
             yield return new WaitForSeconds(waitTime);
-            GameObject newFire = GameObject.Instantiate(fireball, fireballPosition, transform.rotation);
+            GameObject newFire = GameObject.Instantiate(fireball, fireballPosition, player.transform.rotation);
             Destroy(newFire, 5.0f);
-    }
-
-    private IEnumerator ClearFireball(float waitTime, Vector3 fireballPosition)
-    {
-
-        yield return new WaitForSeconds(waitTime);
-        GameObject newFire = GameObject.Instantiate(clearFire, fireballPosition, transform.rotation);
-        Destroy(newFire, 2.0f);
     }
 
     private void ThrowFireball()
     {
-        Vector3 fireballPosition = player.transform.position;
-        GameObject newPreFire = GameObject.Instantiate(preFireball, fireballPosition, transform.rotation);
+        Vector3 fireballPosition = new Vector3(player.transform.position.x, 0.05f, player.transform.position.z);
+        GameObject newPreFire = GameObject.Instantiate(preFireball, fireballPosition, player.transform.rotation);
         Destroy(newPreFire, 2.0f);
         StartCoroutine(Fireball(2.0f, fireballPosition));
-        StartCoroutine(ClearFireball(6.0f, fireballPosition));
     }
 
     private void ThrowFirewall()
     {
 
-        GameObject newFire = GameObject.Instantiate(firewall, transform.position, transform.rotation);
+        GameObject newFire = GameObject.Instantiate(firewall, thrower.transform.position, transform.rotation);
+        Destroy(newFire, 4.0f);
     }
 
     string output;
