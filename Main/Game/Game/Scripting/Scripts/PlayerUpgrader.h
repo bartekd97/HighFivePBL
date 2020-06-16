6244b30269b208040aea3ab9591bbf23821c8b65
Bartek <bartekd97@gmail.com>
2020-06-16 02:25:30 +0200

    Upgrades bar

--- Main/Game/Game/Scripting/Scripts/PlayerUpgrader.h
+++ Main/Game/Game/Scripting/Scripts/PlayerUpgrader.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 #include <random>
 #include <glm/glm.hpp>
 #include <glm/gtx/easing.hpp>
@@ -18,6 +19,7 @@ public:
 	struct UpgradeButton
 	{
 		std::shared_ptr<Button> button;
+		std::shared_ptr<Panel> buttonIcon;
 		std::shared_ptr<Prefab> upgradePrefab;
 	};
 
@@ -34,7 +36,13 @@ private: // variables
 	std::shared_ptr<Label> descriptionLabel;
 	bool canClickUpgradeButton = false;
 
+	std::shared_ptr<Panel> upgradesBarAvatarPanel;
+	std::shared_ptr<Panel> upgradesBarPanel;
+	std::vector<std::shared_ptr<Panel>> upgradesBarIcons;
+	std::shared_ptr<Panel> upgradeAnimIconPanel;
+
 	std::vector<std::shared_ptr<Prefab>> upgradePrefabs;
+	std::unordered_map<std::string, std::shared_ptr<Texture>> iconTexturesCache;
 
 	TimerAnimator timerAnimator;
 
@@ -46,13 +54,22 @@ private: // variables
 		button->SetPivot(Anchor::CENTER);
 		button->SetPosition({ posX, buttonYPos, 0.0f });
 		button->SetSize(upgradeButtonSize);
-		button->textureColors[Button::STATE::NORMAL].color = glm::vec4(0.7f, 0.7f, 0.7f, 0.7f);
-		button->textureColors[Button::STATE::HOVER].color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+		button->textureColors[Button::STATE::NORMAL].texture = TextureManager::GetTexture("GUI/Upgrades", "CardButton_Normal");
+		button->textureColors[Button::STATE::HOVER].texture = TextureManager::GetTexture("GUI/Upgrades", "CardButton_Hover");
 		button->OnStateChanged = std::bind(&PlayerUpgrader::ButtonStateChanged, this, index, std::placeholders::_1);
 		button->OnClickListener = std::bind(&PlayerUpgrader::ButtonClicked, this, index);
 
+		auto buttonIcon = std::make_shared<Panel>();
+		buttonIcon->SetCoordinatesType(Widget::CoordinatesType::RELATIVE);
+		buttonIcon->SetAnchor(Anchor::CENTER);
+		buttonIcon->SetPivot(Anchor::CENTER);
+		buttonIcon->SetPosition({ 0.0f, 0.0f, 0.0f });
+		buttonIcon->SetSize({1.0f, 1.0f});
+
 		upgradeButtons[index].button = button;
+		upgradeButtons[index].buttonIcon = buttonIcon;
 		GUIManager::AddWidget(button, upgradePanel);
+		GUIManager::AddWidget(buttonIcon, button);
 	}
 
 public:
@@ -87,11 +104,55 @@ public:
 
 		upgradePanel->SetEnabled(false);
 
+		// create bar with avatar and icons
+		upgradesBarPanel = std::make_shared<Panel>();
+		upgradesBarPanel->SetCoordinatesType(Widget::CoordinatesType::RELATIVE);
+		upgradesBarPanel->SetPivot(Anchor::BOTTOMLEFT);
+		upgradesBarPanel->SetPositionAnchor({ 0.045f, -0.06f, 0.0f }, Anchor::BOTTOMLEFT);
+		upgradesBarPanel->SetSize(glm::vec2{ 0.357f, 0.0695f } * 1.0f);
+		upgradesBarPanel->textureColor.texture = TextureManager::GetTexture("GUI/Upgrades", "UpgradesBar");
+		GUIManager::AddWidget(upgradesBarPanel);
+
+		upgradesBarAvatarPanel = std::make_shared<Panel>();
+		upgradesBarAvatarPanel->SetCoordinatesType(Widget::CoordinatesType::RELATIVE);
+		upgradesBarAvatarPanel->SetPivot(Anchor::CENTER);
+		upgradesBarAvatarPanel->SetPositionAnchor({ 0.0f, 0.5f, 0.0f }, Anchor::TOPLEFT);
+		upgradesBarAvatarPanel->SetSize(glm::vec2{ 0.165f, 1.5f } * 1.2f);
+		upgradesBarAvatarPanel->textureColor.texture = TextureManager::GetTexture("GUI/Upgrades", "UpgradesBarAvatar");
+		GUIManager::AddWidget(upgradesBarAvatarPanel, upgradesBarPanel);
+
+		for (int i = 0; i < 8; i++)
+		{
+			auto iconPanel = std::make_shared<Panel>();
+			iconPanel->SetCoordinatesType(Widget::CoordinatesType::RELATIVE);
+			iconPanel->SetPivot(Anchor::CENTER);
+			iconPanel->SetPositionAnchor({ 0.165f + 0.105f*float(i), 0.5f, 0.0f }, Anchor::TOPLEFT);
+			iconPanel->SetSize(glm::vec2{ 0.0825f, 0.75f } * 0.95f);
+			iconPanel->SetEnabled(false);
+			upgradesBarIcons.push_back(iconPanel);
+			GUIManager::AddWidget(iconPanel, upgradesBarPanel);
+		}
+
+		// create icon for animating
+		upgradeAnimIconPanel = std::make_shared<Panel>();
+		upgradeAnimIconPanel->SetEnabled(false);
+		GUIManager::AddWidget(upgradeAnimIconPanel, nullptr, 2);
+
+
+		// setup upgrade prefabs
 		upgradePrefabs = {
 			PrefabManager::GetPrefab("Upgrades/GhostDistance"),
 			PrefabManager::GetPrefab("Upgrades/GhostRecovery"),
 			PrefabManager::GetPrefab("Upgrades/GhostSpeed"),
 		};
+		
+		// cache upgrade icons
+		for (auto& upgrade : upgradePrefabs)
+		{
+			std::string iconName;
+			upgrade->Properties().GetString("icon", iconName);
+			iconTexturesCache[iconName] = TextureManager::GetTexture("GUI/Upgrades", iconName);
+		}
 	}
 
 	void ButtonStateChanged(int index, Button::STATE newState)
@@ -140,6 +201,42 @@ public:
 		timerAnimator.AnimateVariable(&upgradePanel->textureColor.color.a, upgradePanel->textureColor.color.a, 0.0f, 0.5f);
 		timerAnimator.DelayAction(1.5f, [&]() {upgradePanel->SetEnabled(false);});
 
+
+		std::shared_ptr<Panel> freeIconSlot;
+		for (auto& iconSlot : upgradesBarIcons)
+		{
+			if (!iconSlot->GetEnabled())
+			{
+				freeIconSlot = iconSlot;
+				break;
+			}
+		}
+		if (freeIconSlot)
+		{
+			glm::vec3 posFrom = upgradeButtons[index].buttonIcon->GetAbsolutePosition();
+			glm::vec2 scaleFrom = upgradeButtons[index].buttonIcon->GetLocalSize();
+			glm::vec3 posTo = freeIconSlot->GetAbsolutePosition();
+			glm::vec2 scaleTo = freeIconSlot->GetLocalSize();
+
+			upgradeAnimIconPanel->SetEnabled(true);
+			upgradeAnimIconPanel->textureColor.texture = upgradeButtons[index].buttonIcon->textureColor.texture;
+			timerAnimator.UpdateInTime(1.2f, [&, posFrom, scaleFrom, posTo, scaleTo](float prog) {
+				upgradeAnimIconPanel->SetPosition(
+					glm::mix(posFrom, posTo, glm::sineEaseInOut(prog))
+					);
+				upgradeAnimIconPanel->SetSize(
+					glm::mix(scaleFrom, scaleTo, glm::sineEaseInOut(prog))
+					);
+				});
+			timerAnimator.DelayAction(1.2f, [&, freeIconSlot, index]() {
+				upgradeAnimIconPanel->SetEnabled(false);
+				freeIconSlot->SetEnabled(true);
+				freeIconSlot->textureColor.texture = upgradeButtons[index].buttonIcon->textureColor.texture;
+				});
+
+			upgradeButtons[index].buttonIcon->textureColor.color.a = 0.25f;
+		}
+
 		canClickUpgradeButton = false;
 	}
 
@@ -155,6 +252,11 @@ public:
 			std::uniform_int_distribution<> dis(0, upgrades.size() - 1);
 			int ui = dis(gen);
 			upgradeButtons[i].upgradePrefab = upgrades[ui];
+
+			std::string iconName;
+			upgrades[ui]->Properties().GetString("icon", iconName);
+			upgradeButtons[i].buttonIcon->textureColor.texture = iconTexturesCache[iconName];
+			upgradeButtons[i].buttonIcon->textureColor.color.a = 1.0f;
 			upgrades.erase(upgrades.begin() + ui);
 		}
 
