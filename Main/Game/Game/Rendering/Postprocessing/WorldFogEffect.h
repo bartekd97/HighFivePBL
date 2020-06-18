1ffc08e62ccf7699b1950e0992b4167679f40600
Bartek <bartekd97@gmail.com>
2020-06-19 00:17:21 +0200

    Initial world fog effect

---
+++ Main/Game/Game/Rendering/Postprocessing/WorldFogEffect.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include "IPostprocessingEffect.h"
+
+class Shader;
+class Mesh;
+class Event;
+
+class WorldFogEffect : public IPostprocessingEffect
+{
+public:
+	const float FogHeightLevel = 3.5f;
+
+private:
+	std::shared_ptr<Shader> shader;
+	std::shared_ptr<Mesh> fogPlane;
+	std::shared_ptr<Texture> noise;
+
+	glm::vec2 noiseOffsets[3];
+
+	void LateUpdateNoise(Event& event);
+
+public:
+	glm::vec2 noiseSpeeds[3] = {
+		glm::vec2(-0.015f, 0.0f) * 0.5f,
+		glm::vec2(-0.075f, 0.015f) * 0.5f,
+		glm::vec2(0.0f, -0.015f) * 0.5f
+	};
+
+	virtual void Init() override;
+	virtual bool Process(
+		std::shared_ptr<FrameBuffer> source,
+		std::shared_ptr<FrameBuffer> destination,
+		RenderPipeline::GBufferStruct& gbuffer) override;
+};
